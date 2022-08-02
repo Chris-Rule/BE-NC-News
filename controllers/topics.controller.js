32387aac91654408ad4b1a211521488fd5e8f7ea
Chris-Rule <rule-@hotmail.com>
2022-08-02 15:30:16 +0100

    implement PATCH on /api/articles/:article_id

--- controllers/topics.controller.js
+++ controllers/topics.controller.js
@@ -1,4 +1,4 @@
-const { selectTopics, selectArticleById } = require("../models/topics.model")
+const { selectTopics, selectArticleById, updateArticleVotesById } = require("../models/topics.model")
 
 exports.getTopics = (req, res, next) => {
     selectTopics().then((topicData) => {
@@ -11,6 +11,15 @@ exports.getArticleById = (req, res, next) => {
     const {article_id: targetArticleId} = req.params;
     selectArticleById(targetArticleId).then(([articleData]) => {
         res.status(200).send({article: articleData});
+    })
+    .catch(next);
+}
+
+exports.patchArticleVotesById = (req, res, next) => {
+    const inc_votes = req.body;
+    const {article_id: targetArticleId} = req.params;
+    updateArticleVotesById(targetArticleId, inc_votes).then(([articleData]) => {
+        res.status(201).send({article: articleData});
     })
     .catch(next);
 }
