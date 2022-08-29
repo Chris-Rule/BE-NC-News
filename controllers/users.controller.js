b49779cb607ce8e880b86b5bd407a2ab0192f65e
Chris-Rule <rule-@hotmail.com>
2022-08-29 11:17:01 +0100

    api/users/:username endpoint implemented

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,8 +1,14 @@
-const { selectUsers } = require("../models/users.model");
+const { selectUsers, selectUserByUsername } = require("../models/users.model");
 
 exports.getUsers = (req, res, next) => {
     selectUsers().then((userData) => {
         res.status(200).send({users: userData});
-    })
-    .catch(next);
+    }).catch(next);
+}
+
+exports.getUserByUsername = (req,res,next) => {
+    selectUserByUsername(req.params.username)
+    .then(([user]) => {
+        res.status(200).send({user});
+    }).catch(next);
 }
